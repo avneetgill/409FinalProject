ebb6da65812ae853649989d49cafcf17e5197214
avneetgill <avneet2.gill@gmail.com>
2019-04-03 18:08:43 -0600

    worked on 'add item' GUI

--- FinalTool2.0/Client/View/AddView.java
+++ FinalTool2.0/Client/View/AddView.java
@@ -6,6 +6,7 @@ import java.awt.Font;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import javax.swing.*;
+
 public class AddView extends JFrame{
     JPanel headerPanel = new JPanel();
     JPanel textFields = new JPanel();
