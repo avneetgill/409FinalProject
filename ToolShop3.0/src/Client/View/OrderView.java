342f6e485bf03ba03e7d86d9b70510e76b0ddc14
avneetgill <avneet2.gill@gmail.com>
2019-04-10 22:29:46 -0600

    added order view

---
+++ ToolShop3.0/src/Client/View/OrderView.java
@@ -0,0 +1,67 @@
+package Client.View;
+
+import java.awt.BorderLayout;
+import java.awt.Font;
+import javax.swing.*;
+import javax.swing.border.Border;
+
+import java.awt.Color;
+
+
+
+public class OrderView extends JFrame{
+	/**
+     * Main Label for  student record maintainer
+     */
+	private JLabel title = new JLabel ("Orders");
+	/**
+     * TextArea that displays all students when browse is pressed. 
+     */
+	private JTextArea textBox = new JTextArea(10, 30);
+    
+	public OrderView(){
+        JPanel mainPanel = new JPanel ();
+        JPanel topTitle = new JPanel();
+     
+        setTitle("Main");
+        this.setBackground(Color.WHITE);
+        this.setSize(700, 400);
+        this.setResizable(false);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        setLayout(new BorderLayout());	// doesnt do jack
+        
+        title.setFont(new Font("Sans", Font.BOLD, 18));
+        topTitle.add(title);
+
+       
+
+        textBox.setEditable(false);
+        Border border = BorderFactory.createLineBorder(Color.gray, 10);
+        textBox.setBorder(border);
+        JScrollPane scrollText = new JScrollPane(textBox, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        mainPanel.setLayout(new BorderLayout());
+        mainPanel.add(scrollText, BorderLayout.CENTER);
+        
+        add("Center", mainPanel);
+        add("North", topTitle);
+    }
+    /**
+     * displays String in the TextArea 
+     * @param text - text to be displayed
+     */
+    public void setTextArea(String text){
+        textBox.setText(text);
+    }
+    
+    public void errorMessage(String error){
+        JOptionPane.showMessageDialog(this, error);
+    }
+    
+    public static void main(String[] args) {
+        OrderView o = new OrderView();
+        o.setVisible(true);
+        o.setTextArea("yes\n no\n yes\n no\nyes\n no\nyes\n no\nyes\n no\nyes\n no\nyes\n no\nyes\n no\nyes\n no\nyes\n no\nyes\n no\nyes\n no\n");
+
+    }
+}
